12ba6b3488ce7131e53afe4f3919d8dfadb8bcac
Lex <lu.vuiton.is@gmail.com>
2026-06-25 19:08:03 -0600

    correccion de JDK 25 a 21

--- src/main/java/com/verassoft/evaluaciones/microservvicios/paq/ClaseA.java
+++
@@ -1,8 +0,0 @@
-package com.verassoft.evaluaciones.microservvicios.paq;
-
-public class ClaseA {
-
-    public String compararTipo(ClaseB claseB) {
-        return "eeee"+claseB.concatenar("BBB");
-    }
-}
